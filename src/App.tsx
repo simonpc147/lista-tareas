69eb1fae585b6637c76745ebba7481d61feb63e6
Simon Parra <alexparracol81@gmail.com>
2024-06-12 21:23:30 -0400

    primer commit

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import Title from "./components/Title";
+
+function App(){
+
+  return <Title />;
+}
+
+export default App;
---
+++ src/components/Title.tsx
@@ -0,0 +1,11 @@
+function Title(){
+
+    const nombre = "SIMITO";
+    return(    
+        <h1 className="flex justify-center items-center w-full h-lvh">
+        {nombre}
+      </h1>
+    )
+  }
+  
+  export default Title;
